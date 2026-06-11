3459852103746f0618d50ff2c05e0d5aa9ac07a4
mohamedelbendary <mohamedelbendary228@gmail.com>
2026-06-11 18:22:29 +0300

    Add Firebase dependancies

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,11 +1,3 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import CLTypingLabel
 import UIKit
 
