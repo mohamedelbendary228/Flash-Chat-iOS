0229756e75c5eaa0fc140bda27fcf9d1ee7a331f
mohamedelbendary <mohamedelbendary228@gmail.com>
2026-06-14 15:33:03 +0300

    Add a custom tabelViewCell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -5,11 +5,20 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    var messages: [Message] = [
+        Message(sender: "test@gmail.com", body: "Hey"),
+        Message(sender: "another@gmail.com", body: "Hello"),
+        Message(sender: "test@gmail.com", body: "How are you?")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "⚡️FlashChat"
+        tableView.dataSource = self
+        title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
 
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -24,5 +33,21 @@ class ChatViewController: UIViewController {
           print("Error signing out: %@", signOutError)
         }
     }
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+            as! MessageCell
+        cell.label?.text = messages[indexPath.row].body
+        
+        return cell
+    }
+    
     
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,7 +7,7 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = "⚡️FlashChat"
+        titleLabel.text = K.appName
     }
 
 }
